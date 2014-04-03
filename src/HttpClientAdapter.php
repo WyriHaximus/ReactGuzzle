efa0abffd5b7201cc4c8a8110104a22974f2689b
Cees-Jan Kiewiet <wyri@haxim.us>
2014-04-03 09:15:26 +0200

    Setting resolver and HTTP client on instantiation

--- src/HttpClientAdapter.php
+++ src/HttpClientAdapter.php
@@ -2,18 +2,29 @@
 
 namespace WyriHaximus\React\Guzzle;
 
-use \React\Promise\Deferred;
+use \GuzzleHttp\Adapter\AdapterInterface;
+use \GuzzleHttp\Adapter\TransactionInterface;
+use \GuzzleHttp\Message\MessageFactory;
+use \React\Dns\Resolver as DnsResolver;
+use \React\Dns\Resolver\Factory as DnsFactory;
+use \React\EventLoop\LoopInterface;
 use \React\HttpClient\Client as HttpClient;
 use \React\HttpClient\Factory as HttpClientFactory;
 use \React\HttpClient\Response as HttpResponse;
-use \React\Dns\Resolver as DnsResolver;
-use \React\Dns\Resolver\Factory as DnsFactory;
-use \GuzzleHttp\Message\MessageFactory;
+use \React\Promise\Deferred;
 
-class HttpClientAdapter implements \GuzzleHttp\Adapter\AdapterInterface {
 
-    public function __construct(\React\EventLoop\LoopInterface $loop) {
+class HttpClientAdapter implements AdapterInterface {
+
+    protected $dnsResolver;
+    protected $httpClient;
+
+    public function __construct(LoopInterface $loop, HttpClient $httpClient = null, DnsResolver $dnsResolver = null) {
+        $this->loop = $loop;
         $this->messageFactory = new MessageFactory();
+
+        $this->setDnsResolver($dnsResolver);
+        $this->setHttpClient($httpClient);
     }
 
     public function setHttpClient(HttpClient $httpClient = null) {
@@ -36,9 +47,9 @@ class HttpClientAdapter implements \GuzzleHttp\Adapter\AdapterInterface {
         $this->dnsResolver = $dnsResolver;
     }
 
-    public function send(\GuzzleHttp\Adapter\TransactionInterface $transaction) {
+    public function send(TransactionInterface $transaction) {
         $deferred = new Deferred();
-        $request = $this->client->request($transaction->getRequest()->getMethod(), $transaction->getRequest()->getUrl(), $transaction->getRequest()->getHeaders());
+        $request = $this->httpClient->request($transaction->getRequest()->getMethod(), $transaction->getRequest()->getUrl(), $transaction->getRequest()->getHeaders());
 
         $httpResponse = null;
         $buffer = '';
