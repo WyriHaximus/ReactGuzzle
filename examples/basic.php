e0e112d1d4549e9b18b22618c1cdc67073ede966
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-04-09 23:04:49 +0200

    Basic example, more will follow

---
+++ examples/basic.php
@@ -0,0 +1,18 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Create eventloop
+$loop = \React\EventLoop\Factory::create();
+
+(new \GuzzleHttp\Client([
+    'adapter' => new \WyriHaximus\React\Guzzle\HttpClientAdapter($loop),
+]))->get('http://docs.guzzlephp.org/en/latest/')->then(function(\GuzzleHttp\Message\Response $response) { // Success callback
+    var_export($response);
+}, function($event) { // Error callback
+    var_export($event);
+}, function(array $event) { // Progress callback
+    var_export($event);
+});
+
+$loop->run();
