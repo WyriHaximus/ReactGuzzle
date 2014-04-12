ed0ac111af989b1ee6735a7c81113e75057e3af3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-04-12 22:18:44 +0200

    Create RequestFactory test

---
+++ tests/HttpClient/RequestFactoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of ReactGuzzle.
+ *
+ ** (c) 2014 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\React\Tests\Guzzle\HttpClient;
+
+use WyriHaximus\React\Guzzle\HttpClient\RequestFactory;
+
+/**
+ * Class RequestFactoryTest
+ *
+ * @package WyriHaximus\React\Tests\Guzzle
+ */
+class RequestFactoryTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->requestFactory = new RequestFactory();
+    }
+    
+    public function tearDown() {
+        parent::tearDown();
+        
+        unset($this->requestFactory);
+    }
+
+    public function testCreate() {
+        $this->assertInstanceOf('WyriHaximus\React\Guzzle\HttpClient\Request', $this->requestFactory->create($this->getMock('React\HttpClient\Client', [], [
+            $this->getMock('React\SocketClient\ConnectorInterface'),
+            $this->getMock('React\SocketClient\ConnectorInterface'),
+        ])));
+    }
+    
+}
