48ab7b85f6290155550ede5771e3086be54e02eb
Cees-Jan <ceesjank@gmail.com>
2014-04-13 02:40:07 +0200

    Basic Request tests

--- tests/HttpClient/RequestTest.php
+++ tests/HttpClient/RequestTest.php
@@ -10,8 +10,6 @@
  */
 namespace WyriHaximus\React\Tests\Guzzle\HttpClient;
 
-use WyriHaximus\React\Guzzle\HttpClient\RequestFactory;
-
 /**
  * Class Requesttest
  *
@@ -34,15 +32,105 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
     public function tearDown() {
         parent::tearDown();
         
-        unset($this->requestFactory);
+        unset($this->httpClient, $this->request);
     }
 
     public function testSend() {
-        /*$this->httpClient->expects($this->once())
+        $headersGuzzle = [
+            'X-Guzzle' => [
+                'React',
+                'HttpClient',
+            ],
+        ];
+        $headers = [
+            'X-Guzzle' => 'React, HttpClient',
+        ];
+        $method = \GuzzleHttp\Message\RequestInterface::GET;
+        $url = 'http://example.com/';
+
+        $deferred = new \React\Promise\Deferred();
+
+        $connector = $this->getMock('React\SocketClient\ConnectorInterface', [
+            'create',
+        ]);
+        $connector->expects($this->at(0))
             ->method('create')
-            ->with($this->httpClient)
-            ->willReturn('');
-        $this->request->send($this->getMock('GuzzleHttp\Adapter\TransactionInterface'));*/
+            ->willReturn($deferred->promise());
+
+        $httpRequest = $this->getMock('React\HttpClient\Request', [
+                'trigger',
+            ], [
+            $connector,
+            $this->getMock('React\HttpClient\RequestData', [], [
+                $method,
+                $url,
+                $headers,
+            ]),
+        ]);
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with($method, $url, $headers)
+            ->willReturn($httpRequest);
+
+        $request = $this->getMock('GuzzleHttp\Message\RequestInterface', [
+            '__toString',
+            'setUrl',
+            'getUrl',
+            'getResource',
+            'getQuery',
+            'setQuery',
+            'getMethod',
+            'setMethod',
+            'getScheme',
+            'setScheme',
+            'getHost',
+            'setHost',
+            'getPath',
+            'setPath',
+            'getConfig',
+            'getProtocolVersion',
+            'setBody',
+            'getBody',
+            'getHeaders',
+            'getHeader',
+            'hasHeader',
+            'removeHeader',
+            'addHeader',
+            'addHeaders',
+            'setHeader',
+            'setHeaders',
+            'getEmitter',
+        ]);
+        $request->expects($this->once())
+            ->method('getHeaders')
+            ->with()
+            ->willReturn($headersGuzzle);
+        $request->expects($this->once())
+            ->method('getMethod')
+            ->with()
+            ->willReturn($method);
+        $request->expects($this->once())
+            ->method('getUrl')
+            ->with()
+            ->willReturn($url);
+        $request->expects($this->once())
+            ->method('getHeader')
+            ->with('X-Guzzle')
+            ->willReturn($headers['X-Guzzle']);
+
+        $transaction = $this->getMock('GuzzleHttp\Adapter\TransactionInterface', [
+            'getRequest',
+            'getResponse',
+            'setResponse',
+            'getClient',
+        ]);
+        $transaction->expects($this->once())
+            ->method('getRequest')
+            ->with()
+            ->willReturn($request);
+
+        $this->request->send($transaction);
     }
     
 }
